d3141a7f15a89e7c586cdd93f69129ed2fb4b1d9
Théo <kinder.theo@gmail.com>
2020-10-08 03:18:51 +0200

    add choice to form user

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -23,6 +24,13 @@ class UserType extends AbstractType
                 'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
             ])
             ->add('email', EmailType::class, ['label' => 'Adresse email'])
+            ->add("userRole", ChoiceType::class, [
+                'choices' => [
+                    "Admin" => "ROLE_ADMIN",
+                    "User" => "ROLE_USER"
+                ],
+                'label' => "Rôle"
+            ])
         ;
     }
 }
